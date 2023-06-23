96ab3f5e3d0656777f45cf30067c5417e898c405
BilankuluClyde <mpfunobilac@gmail.com>
2023-06-24 00:58:20 +0200

    Updated GUI to have functional screeen size changing buttons

--- src/src/HomeScreen.java
+++ src/src/HomeScreen.java
@@ -1,6 +1,8 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HomeScreen extends JFrame {
 
@@ -32,6 +34,59 @@ public class HomeScreen extends JFrame {
         // Now the custom size button
 
         // Finally the custom button
+        JButton[] btns = {new JButton("Full Screen"), new JButton("Half Screen"),
+                new JButton("Minimize Screen"), new JButton("Custom size Screen")};
+        btns[0].addActionListener
+                (
+                        new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                wSize = ScreenSize.FullScreen;
+                                frame.setSize(wSize.dim_Size());
+                            }
+                        }
+                );
+        btns[1].addActionListener
+                (
+                        new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                wSize = ScreenSize.HalfScreen;
+                                frame.setSize(wSize.dim_Size());
+                            }
+                        }
+                );
+        btns[2].addActionListener
+                (
+                        new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                wSize = ScreenSize.Minimize;
+                                frame.setSize(wSize.dim_Size());
+                            }
+                        }
+                );
+        btns[3].addActionListener
+                (
+                        new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                wSize = ScreenSize.Custom;
+                                frame.setSize(wSize.dim_Size());
+                            }
+                        }
+                );
+
+        JPanel btnPane = new JPanel();
+
+        for (JButton btn : btns) {
+            btnPane.add(btn, -1);
+        }
+
+        frame.getContentPane().add(btnPane, BorderLayout.SOUTH);
+        frame.validate();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     public void run() {
